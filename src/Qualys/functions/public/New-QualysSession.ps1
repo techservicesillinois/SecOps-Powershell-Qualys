ae8227e4f7405a6b799569cbe694ebfe6f2abadf
Buch <buch1@illinois.edu>
2020-10-15 14:22:40 -0500

    satisfy SupportsShouldProcess

--- src/Qualys/functions/public/New-QualysSession.ps1
+++ src/Qualys/functions/public/New-QualysSession.ps1
@@ -17,27 +17,30 @@ function New-QualysSession{
     )
 
     process{
-        $IVRSplat = @{
-            Headers = @{
-                "X-Requested-With"="powershell"
-            }
-            Method = 'POST'
-            URI = "$($Script:Settings.BaseURI)session/"
-            Body = @{
-                action = "login"
-                username = $Credential.UserName
-                password = $Credential.GetNetworkCredential().Password
-            }
-            SessionVariable = 'Cookie'
-        }
 
-        Write-Verbose -Message "Generating Qualys API cookie for $($Credential.UserName)"
-        $Response = Invoke-RestMethod @IVRSplat
-        if( $Response.SIMPLE_RETURN.RESPONSE.TEXT -eq 'Logged in' ){
-            $Script:Session = $Cookie
-        }
-        else{
-            Throw $Response.SIMPLE_RETURN.RESPONSE.TEXT
+        if ($PSCmdlet.ShouldProcess($Credential)){
+            $IVRSplat = @{
+                Headers = @{
+                    "X-Requested-With"="powershell"
+                }
+                Method = 'POST'
+                URI = "$($Script:Settings.BaseURI)session/"
+                Body = @{
+                    action = "login"
+                    username = $Credential.UserName
+                    password = $Credential.GetNetworkCredential().Password
+                }
+                SessionVariable = 'Cookie'
+            }
+
+            Write-Verbose -Message "Generating Qualys API cookie for $($Credential.UserName)"
+            $Response = Invoke-RestMethod @IVRSplat
+            if( $Response.SIMPLE_RETURN.RESPONSE.TEXT -eq 'Logged in' ){
+                $Script:Session = $Cookie
+            }
+            else{
+                Throw $Response.SIMPLE_RETURN.RESPONSE.TEXT
+            }
         }
     }
 }
