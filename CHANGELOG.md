cb097aa838d118fce0ead546764d9912e57c7e75
Tamara Buch <git@tamarabee.com>
2021-12-01 11:14:22 -0600

    Fix ip output (#48)
    * add function
    
    * update params for psscriptanalyzer
    
    * update params for psscriptanalyzer
    
    * revert to original syntax
    
    * fix comment based help
    
    * Update Markdown Help
    
    * changelog++
    
    * changelog++
    
    * changelog++
    
    * version++
    
    * close-qualyssession
    
    * capitalize weekdays
    
    * bug #42
    
    * changelog and version ++
    
    * changelog++
    
    * update markdown
    
    * fix parameter set
    
    * add parameter sets
    
    * add parameter sets
    
    * update markdown help
    
    * move websession into ivrsplat
    
    * fix DefaultParameterSetName
    
    * changelog++
    
    * moduleversion++
    
    * fix IP logic in get-qualysassetgroups
    
    * readability changes
    
    * readability improvements
    
    * store part of response in new var

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Removed
 
+## [1.4.3] - 2021-11-23
+
+### Changed
+
+- IP logic in Get-QualysAssetGroups sometimes resulted in a string being returned instead of a string array for the IPs property
+
 ## [1.4.2] - 2021-10-14
 
 ### Added
--- src/UofIQualys/functions/public/Get-QualysAssetGroups.ps1
+++ src/UofIQualys/functions/public/Get-QualysAssetGroups.ps1
@@ -49,50 +49,58 @@ function Get-QualysAssetGroups{
             }
         }
 
-        $IPs = @()
         $Response = Invoke-QualysRestCall @RestSplat
+        #This will return IP information for every asset group
         if(!($Identity)){
             $Index = 0
-            foreach ($ID in $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.ID ) {
-                If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP_RANGE){
-                    $IPs = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP_RANGE
-                    If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP){
-                        $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP
+            $AssetGroupInfo = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP
+            foreach ($ID in $AssetGroupInfo.ID) {
+                $IPs = @()
+                #If there are ranges get the ranges and add them to the IP array
+                If($AssetGroupInfo.IP_SET[$Index].IP_RANGE){
+                    $IPs += $AssetGroupInfo.IP_SET[$Index].IP_RANGE
+                    #If there are IPs and ranges now add the IPs to the array with the ranges
+                    If($AssetGroupInfo.IP_SET[$Index].IP){
+                        $IPs += $AssetGroupInfo.IP_SET[$Index].IP
                     }
                 }
-                ElseIf($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP){
-                    $IPs = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP
+                #Sometimes there are only IPs and no ranges, add the IPs to the array
+                ElseIf($AssetGroupInfo.IP_SET[$Index].IP){
+                    $IPs += $AssetGroupInfo.IP_SET[$Index].IP
                 }
                 $AssetGroup = [PSCustomObject]@{
                     ID = $ID
-                    Title = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.TITLE.'#cdata-section'[$Index]
+                    Title = $AssetGroupInfo.TITLE.'#cdata-section'[$Index]
                     IP_Range = $IPs
-                    DefaultScanner = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.DEFAULT_APPLIANCE_ID[$Index]
+                    DefaultScanner = $AssetGroupInfo.DEFAULT_APPLIANCE_ID[$Index]
                 }
                 $Index++
                 $AssetGroup
             }
         }
+        #Get the IPs for if a single asset group identity is provided with the same logic as above
         else{
-            If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP_RANGE){
-                $IPs = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP_RANGE
-                If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP){
-                    $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP
+            $IPs = @()
+            $AssetGroupInfo = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP
+            If($AssetGroupInfo.IP_SET.IP_RANGE){
+                $IPs += $AssetGroupInfo.IP_SET.IP_RANGE
+                If($AssetGroupInfo.IP_SET.IP){
+                    $IPs += $AssetGroupInfo.IP_SET.IP
                 }
             }
-            ElseIf($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP){
-                $IPs = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP
+            ElseIf($AssetGroupInfo.IP_SET.IP){
+                $IPs += $AssetGroupInfo.IP_SET.IP
             }
             $AssetGroup = [PSCustomObject]@{
-                ID = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.ID
-                Title = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.TITLE.'#cdata-section'
+                ID = $AssetGroupInfo.ID
+                Title = $AssetGroupInfo.TITLE.'#cdata-section'
                 IP_Range = $IPs
-                DefaultScanner = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.DEFAULT_APPLIANCE_ID
+                DefaultScanner = $AssetGroupInfo.DEFAULT_APPLIANCE_ID
             }
             If(!($AssetGroup.ID))
                 {
                     Throw "No Asset Group found matching the title or ID $($Identity)"
-                    Break
+                    Exit
                 }
             $AssetGroup
         }
