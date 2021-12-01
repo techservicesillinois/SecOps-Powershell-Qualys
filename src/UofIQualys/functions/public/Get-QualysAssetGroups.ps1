f8e9ef6977f58525209b9560c0fbf86b0ce25241
baristaTam <git@tamarabee.com>
2021-12-01 11:02:04 -0600

    store part of response in new var

--- src/UofIQualys/functions/public/Get-QualysAssetGroups.ps1
+++ src/UofIQualys/functions/public/Get-QualysAssetGroups.ps1
@@ -53,26 +53,26 @@ function Get-QualysAssetGroups{
         #This will return IP information for every asset group
         if(!($Identity)){
             $Index = 0
-            $AssetGroupIDs = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.ID
-            foreach ($ID in $AssetGroupIDs) {
+            $AssetGroupInfo = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP
+            foreach ($ID in $AssetGroupInfo.ID) {
                 $IPs = @()
                 #If there are ranges get the ranges and add them to the IP array
-                If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP_RANGE){
-                    $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP_RANGE
+                If($AssetGroupInfo.IP_SET[$Index].IP_RANGE){
+                    $IPs += $AssetGroupInfo.IP_SET[$Index].IP_RANGE
                     #If there are IPs and ranges now add the IPs to the array with the ranges
-                    If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP){
-                        $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP
+                    If($AssetGroupInfo.IP_SET[$Index].IP){
+                        $IPs += $AssetGroupInfo.IP_SET[$Index].IP
                     }
                 }
                 #Sometimes there are only IPs and no ranges, add the IPs to the array
-                ElseIf($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP){
-                    $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET[$Index].IP
+                ElseIf($AssetGroupInfo.IP_SET[$Index].IP){
+                    $IPs += $AssetGroupInfo.IP_SET[$Index].IP
                 }
                 $AssetGroup = [PSCustomObject]@{
                     ID = $ID
-                    Title = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.TITLE.'#cdata-section'[$Index]
+                    Title = $AssetGroupInfo.TITLE.'#cdata-section'[$Index]
                     IP_Range = $IPs
-                    DefaultScanner = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.DEFAULT_APPLIANCE_ID[$Index]
+                    DefaultScanner = $AssetGroupInfo.DEFAULT_APPLIANCE_ID[$Index]
                 }
                 $Index++
                 $AssetGroup
@@ -81,20 +81,21 @@ function Get-QualysAssetGroups{
         #Get the IPs for if a single asset group identity is provided with the same logic as above
         else{
             $IPs = @()
-            If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP_RANGE){
-                $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP_RANGE
-                If($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP){
-                    $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP
+            $AssetGroupInfo = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP
+            If($AssetGroupInfo.IP_SET.IP_RANGE){
+                $IPs += $AssetGroupInfo.IP_SET.IP_RANGE
+                If($AssetGroupInfo.IP_SET.IP){
+                    $IPs += $AssetGroupInfo.IP_SET.IP
                 }
             }
-            ElseIf($Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP){
-                $IPs += $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.IP_SET.IP
+            ElseIf($AssetGroupInfo.IP_SET.IP){
+                $IPs += $AssetGroupInfo.IP_SET.IP
             }
             $AssetGroup = [PSCustomObject]@{
-                ID = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.ID
-                Title = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.TITLE.'#cdata-section'
+                ID = $AssetGroupInfo.ID
+                Title = $AssetGroupInfo.TITLE.'#cdata-section'
                 IP_Range = $IPs
-                DefaultScanner = $Response.ASSET_GROUP_LIST_OUTPUT.RESPONSE.ASSET_GROUP_LIST.ASSET_GROUP.DEFAULT_APPLIANCE_ID
+                DefaultScanner = $AssetGroupInfo.DEFAULT_APPLIANCE_ID
             }
             If(!($AssetGroup.ID))
                 {
