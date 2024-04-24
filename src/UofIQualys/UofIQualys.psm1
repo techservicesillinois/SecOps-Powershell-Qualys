6ebc012eec19ac9f8c986d4978670193ab47483e
Carter Kindley <ckindley@umn.edu>
2024-04-24 16:06:25 -0500

    Update psm1 to import classes

--- src/UofIQualys/UofIQualys.psm1
+++ src/UofIQualys/UofIQualys.psm1
@@ -9,3 +9,10 @@ Get-ChildItem -Path $FunctionPath -Filter "*.ps1" -Recurse | ForEach-Object -Pro
     Write-Verbose -Message "Importing $($_.BaseName)"
     . $_.FullName | Out-Null
 }
+
+[String]$ClassPath = Join-Path -Path $PSScriptRoot -ChildPath 'Classes'
+#All class files exported to the shell.
+Get-ChildItem -Path $ClassPath -Filter "*.ps1" -Recurse | ForEach-Object -Process {
+    Write-Verbose -Message "Importing $($_.BaseName)"
+    . $_.FullName | Out-Null
+}
