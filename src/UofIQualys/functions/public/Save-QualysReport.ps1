baf5e54471a0aa95b23f02c21f81da65c924c16f
Tamara Buch <buch1@illinois.edu>
2024-07-03 12:14:56 -0500

    save-qualysreport

---
+++ src/UofIQualys/functions/public/Save-QualysReport.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis
+    Download a saved Qualys report in the user’s account.
+.DESCRIPTION
+    Download a saved Qualys report in the user’s account.
+.PARAMETER ID
+    Specifies the report ID of a saved report that you want to download. The status of the report must be “finished”.
+    IDs can be obtained from Get-QualysReports
+.EXAMPLE
+    Save-QualysReport -ID '36743223'
+#>
+function Get-QualysReports{
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '',
+            Justification = 'This is consistent with the vendors verbiage')]
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$ID
+    )
+
+    process{
+
+        $RestSplat = @{
+            Method = 'GET'
+            RelativeURI = 'report/'
+            Body = @{
+                action = 'fetch'
+                echo_request = '1'
+                id = $ID
+            }
+        }
+
+        $Response = Invoke-QualysRestCall @RestSplat
+        $Response
+    }
+}
